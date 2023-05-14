101348db37c1adc12977470f80b839cd85295db2
yoshimitakuto <129573171+yoshimitakuto@users.noreply.github.com>
2023-05-14 15:05:04 +0000

    Add is_deleted and create action is custamer_state

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :custamer_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -13,12 +13,25 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  # # DELETE /resource/sign_out
+  # # def destroy
+  # #   super
+  # # end
 
-  # protected
+  protected
+
+  def custamer_state
+    ## 【処理内容1】 入力されたemailからアカウントを1件取得
+    @custamer = Custamer.find_by(email: params[:custamer][:email])
+    ## アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@custamer
+    ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+    if @custamer.valid_password?(params[:custamer][:password]) && @custamer.is_deleted == true
+      ## 【処理内容3】
+      flash[:alert] = "退会済みです"
+      redirect_to new_custamer_registration_path
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
