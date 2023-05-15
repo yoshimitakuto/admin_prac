24182941ef078f41d282a30406143cbfbb861db4
yoshimitakuto <129573171+yoshimitakuto@users.noreply.github.com>
2023-05-15 05:06:07 +0000

    completed add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+p '==================== customer create ===================='
+Custamer.create!(
+  email: "test@test.com",
+  last_name: "長野",
+  first_name: "景気",
+  furigana_last_name: "ナガノ",
+  furigana_first_name: "ケイキ",
+  customer_adress: "レイル県レイルス市",
+  post_code: "111-1111",
+  telephone_number: "000-0000-0001",
+  password: "pppppp",
+  is_deleted: false
+)
+Custamer.create!(
+  email: "test1@test.com",
+  last_name: "長野1",
+  first_name: "景気1",
+  furigana_last_name: "ナガノ1",
+  furigana_first_name: "ケイキ1",
+  customer_adress: "レイル県レイルス市1",
+  post_code: "111-1111",
+  telephone_number: "000-0000-0001",
+  password: "pppppp",
+  is_deleted: false
+)
+Custamer.create!(
+  email: "test2@test.com",
+  last_name: "長野2",
+  first_name: "景気2",
+  furigana_last_name: "ナガノ2",
+  furigana_first_name: "ケイキ2",
+  customer_adress: "レイル県レイルス市2",
+  post_code: "111-1111",
+  telephone_number: "000-0000-0001",
+  password: "pppppp",
+  is_deleted: false
+)
+
+p '==================== admin create ===================='
+Admin.create!(email: "admin@gmail.com", password: "pppppp")
