7166eca54a9ced0461cbef6049c638204a7f926e
Takuto Yoshimi <129573171+yoshimitakuto@users.noreply.github.com>
2023-05-15 14:12:49 +0900

    Revert "Add seed data"

--- db/migrate/20230514140552_add_is_deleted_to_custamers.rb
+++
@@ -1,5 +0,0 @@
-class AddIsDeletedToCustamers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :custamers, :is_deleted, :boolean, default: false
-  end
-end
--- test/models/custamer_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class CustamerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
